4ef240d1dbdcae798d267819988b00eb289dd6a2
deyan petrov <deyanp19@hotmail.com>
2018-06-28 23:15:29 -0500

    server

---
+++ db/schema.sql
@@ -0,0 +1,11 @@
+DROP DATABASE IF EXISTS burgers_db;
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers (
+id INT(10) AUTO_INCREMENT NOT NULL PRIMARY KEY,
+burger_name VARCHAR(10) NOT NULL,
+devoured BOOLEAN
+
+);
